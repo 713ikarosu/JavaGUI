85e77127bc84c6ba8ccd1ea51c0ef00b24931856
AzureAD\惣田大勝 <ikarosu0909@gmail.com>
2018-05-27 22:18:41 +0900

    謎エラー

--- 2_event/MyDrawing.java
+++ 2_event/MyDrawing.java
@@ -5,7 +5,7 @@ public class MyDrawing{
   private Color lineColor, fillCollor; // 線の色、塗り絵
   private int LineWidth; // 線の太さ
 
-  private　boolean isDashed = false; // 破線であるかどうか
+  private boolean isDashed = false; // 破線であるかどうか
 
   // 破線状態の切り替え
   public void setDashed(boolean b){
