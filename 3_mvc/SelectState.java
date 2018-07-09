651b33a56130fb3d25a7490c30421d340a68d867
AzureAD\惣田大勝 <ikarosu0909@gmail.com>
2018-07-09 19:10:37 +0900

    no message

---
+++ 3_mvc/SelectState.java
@@ -0,0 +1,16 @@
+public class SelectState extends State {
+  StateManager stateManager;
+  // MyOctagonal octa;
+  // int x1, y1;
+
+  public SelectState(StateManager stateManager){
+    this.stateManager = stateManager;
+  }
+
+  public void mouseDown(int x,int y){
+
+    // stateManager.setSelected(x,y);
+  }
+  public void mouseUp(int x,int y){}
+  public void mouseDrag(int x,int y){}
+}
